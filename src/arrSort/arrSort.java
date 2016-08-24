b48d6269c22ed77553da5682075054739c553663
yinyn0502 <2543826125@qq.com>
2016-08-24 14:24:42 +0800

    使用冒泡对数组进行降序

---
+++ src/arrSort/arrSort.java
@@ -0,0 +1,27 @@
+package arrSort;
+
+import java.util.Arrays;
+
+public class arrSort {
+	public static void main(String[] args) {
+		testArrSort();
+	}
+
+	//使用冒泡对数值进行降序
+	private static void testArrSort() {
+		// TODO Auto-generated method stub
+		int[] arr = { 56, 91, 65, 2, 14 };
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr.length - i - 1; j++) {
+				if (arr[j] < arr[j + 1]) {
+					arr[j] = arr[j] + arr[j + 1];// 利用两个数的和，将两个数进行原地交换，
+					arr[j + 1] = arr[j] - arr[j + 1];
+					arr[j] = arr[j] - arr[j + 1];
+				}
+			}
+		}
+
+		System.out.println(Arrays.toString(arr));
+	}
+
+}
